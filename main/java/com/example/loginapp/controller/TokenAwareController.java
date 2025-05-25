2aa707cb7db53b8ba58b4d3fc6d91cf461fe18f4
Lynn <2111545820@qq.com>
2025-05-25 16:07:47 +0800

    清空文件但保留 commit 历史

--- main/java/com/example/loginapp/controller/TokenAwareController.java
+++
@@ -1,9 +0,0 @@
-package com.example.loginapp.controller;
-
-/**
- * 所有需要接收 JWT token 的控制器都可以实现这个接口，
- * 以便在 BaseViewController 中统一注入 token。
- */
-public interface TokenAwareController {
-    void setAuthToken(String token);
-}
