5b7b6a3137de26e9a15d12e431a060b2fe2e345a
Lynn <lynn@LynndeMacBook-Air.local>
2025-05-25 15:15:42 +0800

    清空文件但保留 commit 历史

--- main/java/com/example/loginapp/a.java
+++
@@ -1,7 +0,0 @@
-package com.example.loginapp;
-
-public class a {
-    public static void main(String[] args) {
-            Launcher.main(args);
-    }
-}
--- src/main/java/com/example/saving/a.java
+++
@@ -1,8 +0,0 @@
-package com.example.saving;
-
-
-public class a {
-    public static void main(String[] args) {
-        SavingApplication.main(args);
-    }
-}
